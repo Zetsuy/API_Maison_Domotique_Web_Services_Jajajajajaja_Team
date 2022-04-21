affce6dd002fc4fdc4634e4e706a5922966ff5d1
Zetsuy <jeremygarciamobile@gmail.com>
2022-04-21 17:03:04 +0200

    Ajout deux decimal converter

--- src/converter/Converter.ts
+++ src/converter/Converter.ts
@@ -8,17 +8,17 @@ export function convert(rawValue: number | boolean, type: String) {
 
         if (type == "TEMPERATURE") { //si temperature
             var T = (rawValue / 1023) * (rangeT[1] - rangeT[0]) + (rangeT[0]); //calcul temperature
-            resultat = T.toString() + "°C";
+            resultat = (Math.round(T*100)/100).toString() + "°C";
         }
 
         else if (type == "HUMIDITY") { //si humidité
             var Hum = (rawValue / 1023) * 100;
-            resultat = Hum.toString() + "%HR"; //calcul 
+            resultat = (Math.round(Hum*100)/100).toString() + "%HR"; //calcul 
         }
 
         else if (type == "BARO") { //si barometre
             var Baro = (rawValue / 1023) * (rangeBar[1] - rangeBar[0]) + (rangeBar[0]); //calcul 
-            resultat = Baro.toString() + "hPA";
+            resultat = (Math.round(Baro*100)/100).toString() + "hPA";
         }
 
         else if (type == "PROXIMITY") { //si proxmite
